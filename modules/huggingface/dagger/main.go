73a11e1e898d074748b2a9988dd8282dc1c5a220
Gorkem Ercan <gorkem.ercan@gmail.com>
2024-07-17 14:35:08 +0300

    disable entrypoint on base container

--- modules/huggingface/dagger/main.go
+++ modules/huggingface/dagger/main.go
@@ -8,18 +8,16 @@ import (
 const (
 	pythonImageRef = "cgr.dev/chainguard/python:latest-dev"
 	localRepoDir   = "./hfrepo"
-	hfCliPath      = "./.local/bin/huggingface-cli"
+	hfCliPath      = "/home/nonroot/.local/bin/huggingface-cli"
 )
 
 type Huggingface struct{}
 
 func (m *Huggingface) baseContainer() *dagger.Container {
-	execOptions := &dagger.ContainerWithExecOpts{
-		SkipEntrypoint: true,
-	}
 	return dag.Container().From(pythonImageRef).
-		WithExec([]string{"pip", "install", "-U", "huggingface_hub[cli]"}, *execOptions).
-		WithExec([]string{"pip", "install", "huggingface_hub[hf_transfer]"}, *execOptions).
+		WithoutEntrypoint().
+		WithExec([]string{"pip", "install", "-U", "huggingface_hub[cli]"} ).
+		WithExec([]string{"pip", "install", "-U", "huggingface_hub[hf_transfer]"} ).
 		WithEnvVariable("HF_HUB_ENABLE_HF_TRANSFER", "1")
 }
 
@@ -29,13 +27,10 @@ func (m *Huggingface) DownloadRepo(ctx context.Context,
 	hfrepo string,
 	// the Huggingface secret token for authentication
 	secret *dagger.Secret) *dagger.Directory {
-	execOptions := &dagger.ContainerWithExecOpts{
-		SkipEntrypoint: true,
-	}
 	return m.baseContainer().
 		WithDirectory(localRepoDir, dag.Directory()).
-		WithSecretVariable("HF_TOKEN", secret).
 		WithWorkdir("/home/nonroot").
-		WithExec([]string{hfCliPath, "download", hfrepo, "--local-dir", localRepoDir, "--token", "$HF_TOKEN"}, *execOptions).
+		WithSecretVariable("HF_TOKEN", secret).
+		WithExec([]string{hfCliPath, "download", hfrepo, "--local-dir", localRepoDir, "--token", "$HF_TOKEN"}).
 		Directory(localRepoDir)
 }
